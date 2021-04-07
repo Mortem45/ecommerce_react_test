97855c23a536d96f690c73d5d0b6d6465a6634c1
Brandon Monterrozo <ste45_007@hotmail.com>
2021-04-07 00:59:33 -0600

    products ready

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Products from './components/Products/Products';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Products></Products>
+      </div>
+    )
+  }
+}
